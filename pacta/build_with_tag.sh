8148529f53d51c362a751d42c4dd681689d16935
Mauro Lepore <maurolepore@gmail.com>
2020-11-11 19:09:49 -0800

    Remove code deling with pacta_web_template.zip (#22)

--- pacta/build_with_tag.sh
+++ pacta/build_with_tag.sh
@@ -76,21 +76,10 @@ do
     rm -rf "$repo"
 done
 
-web_template_zip="pacta_web_template.zip"
-green "Unzipping $web_template_zip ..."
-unzip -q "$web_template_zip"
-echo
-
-image_tar_gz="pacta_web/2dii_pacta.tar.gz"
+image_tar_gz="2dii_pacta.tar.gz"
 green "Saving 2dii_pacta into $image_tar_gz ..."
 docker save 2dii_pacta | gzip -q > "$image_tar_gz"
 echo
-
-web_zip="pacta_web.zip"
-green "Zipping $web_zip ..."
-zip -rq "$web_zip" pacta_web -x ".DS_Store" -x "__MACOSX"
-
-rm -rf pacta_web
 
 green "Done :-)"
 exit 0
